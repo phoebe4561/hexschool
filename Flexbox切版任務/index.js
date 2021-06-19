311afb2a2e81098e130e714fd35b4f1c2632e2e0
Phoebe Pao <phoebe4561@gmail.com>
2021-06-19 14:47:07 +0800

    ajustment

--- Flexbox切版任務/index.js
+++ Flexbox切版任務/index.js
@@ -1,5 +1,5 @@
 let mainNav = document.querySelector(".menu");
-let navBarToggle = document.querySelector(".hamburger-icon");
+let navBarToggle = document.querySelector(".hamburger_icon");
 navBarToggle.addEventListener("click", () => {
   mainNav.classList.toggle("active");
 });

4837532c3ea9212c517a329535e84752beccb738
Phoebe Pao <phoebe4561@gmail.com>
2021-06-23 10:16:44 +0800

    finished index page

---
+++ perfume/scripts/footer.js
@@ -0,0 +1,15 @@
+footer =
+  '<footer class="full_wrap">\
+<div class="wrap">\
+    <div class="footer_logo">\
+        <a href=""><img src="../statics/imgs/perfume-week6/d’Perfumewhite.png" alt="footer-logo"></a>\
+        <p>© 2020. All Rights Reserved.</p>\
+    </div>\
+    <ul class="footer_menu">\
+        <li><a href="">Product</a></li>\
+        <li><a href="">Blog</a></li>\
+        <li><a href="">Contact</a></li>\
+    </ul>\
+</div>\
+</footer>';
+document.write(footer);
---
+++ perfume/scripts/header.js
@@ -0,0 +1,13 @@
+header =
+  '<header class="wrap">\
+<div class="logo">\
+    <a href=""><img src="../statics/imgs/perfume-week6/d’Perfume.png" alt="logo"></a>\
+</div>\
+<ul class="menu">\
+    <li><a href=""><span><i class="fas fa-store-alt"></i></span>Product</a></li>\
+    <li><a href=""><span><i class="fas fa-user"></i></span>log in</a></li>\
+    <li><a href=""><span><i class="fas fa-heart"></i></span>Wishlist</a></li>\
+    <li><a href=""><span><i class="fas fa-shopping-cart"></i></span>Cart(3)</a></li>\
+</ul>\
+</header>';
+document.write(header);

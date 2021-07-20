98e2d5ae4c6cee39214499189b8a10a6a941e7b5
Phoebe Pao <phoebe4561@gmail.com>
2021-07-20 14:29:00 +0800

    revised page index

--- perfume-store/scripts/header.js
+++ perfume-store/scripts/header.js
@@ -1,19 +1,21 @@
 header =
   '<header class="wrap">\
-<div class="logo">\
+<h1 class="logo">\
     <a href=""><img src="../statics/imgs/perfume-week6/d’Perfume.png" alt="logo"></a>\
-</div>\
-<div class="cart_icon">\
-    <i class="fas fa-shopping-cart fs-24"></i><span>3</span>\
-</div>\
-<div class="hamburger_icon">\
-    <i class="fas fa-bars fs-32"></i>\
+</h1>\
+<div class="combo">\
+    <a href="#" class="cart_icon">\
+        <i class="fas fa-shopping-cart fs-28"></i><span>3</span>\
+    </a>\
+    <a href="#" class="hamburger_icon">\
+        <i class="fas fa-bars fs-32"></i>\
+    </a>\
 </div>\
 <ul class="menu">\
-    <li><a class="fs-20 p3" href=""><span class="dp-n"><i class="fas fa-store-alt"></i></span>Product</a></li>\
-    <li><a class="fs-20 p3" href=""><span class="dp-n"><i class="fas fa-user"></i></span>log in</a></li>\
-    <li><a class="fs-20 p3" href=""><span class="dp-n"><i class="fas fa-heart"></i></span>Wishlist</a></li>\
-    <li><a class="fs-20 p3" href=""><span class="dp-n"><i class="fas fa-shopping-cart"></i></span>Cart(3)</a></li>\
+<li><a class="fs-20 p3" href=""><span class="dp-n"><i class="fas fa-store-alt"></i></span>Product</a></li>\
+<li><a class="fs-20 p3" href=""><span class="dp-n"><i class="fas fa-user"></i></span>log in</a></li>\
+<li><a class="fs-20 p3" href=""><span class="dp-n"><i class="fas fa-heart"></i></span>Wishlist</a></li>\
+<li><a class="fs-20 p3" href=""><span class="dp-n"><i class="fas fa-shopping-cart"></i></span>Cart(3)</a></li>\
 </ul>\
 </header>';
 document.write(header);
